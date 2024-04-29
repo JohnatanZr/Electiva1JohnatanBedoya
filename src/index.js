85197b4b17b76db7dd37c84f798556e2085fd66d
JohnatanZr <95395086+JohnatanZr@users.noreply.github.com>
2024-04-29 10:27:17 -0500

    Update import a index

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 import { Giphy } from './Component/Giphy';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
